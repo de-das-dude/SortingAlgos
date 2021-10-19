efd92830354e41c11e7c85ebe917bf5c939a5681
dedas <dedas@DESKTOP-7L7531O>
2021-10-19 12:16:18 +0530

    selection sort

--- src/com/sayan/selectionSort/SelectionSort.java
+++ src/com/sayan/selectionSort/SelectionSort.java
@@ -1,5 +1,29 @@
 package com.sayan.selectionSort;
 
 public class SelectionSort {
+	int[] sortAsc(int[] arr) {
+		int n = arr.length;
 
+		for (int i = 0; i < n - 1; i++) {
+			int min_idx = i;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[min_idx] > arr[j])
+					min_idx = j;
+			}
+			int temp = arr[i];
+			arr[i] = arr[min_idx];
+			arr[min_idx] = temp;
+		}
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		SelectionSort sort = new SelectionSort();
+		int[] arr = { 5, 2, 6, 7, 2, 5, 9, 45 };
+
+		arr = sort.sortAsc(arr);
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+	}
 }
